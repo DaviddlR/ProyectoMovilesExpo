e4df3d80acd6735e6b910586a4cb3ee4bc8f3739
David de la Rosa <drr00025@red.ujaen.es>
2022-12-06 13:39:26 +0100

    Pantalla menu principal acabada

--- componentes/MainMenu.js
+++ componentes/MainMenu.js
@@ -4,6 +4,8 @@ import React, { useState } from 'react';
 import {
   View,
   Text,
+  StyleSheet,
+  TouchableOpacity,
 } from 'react-native';
 
 import {
@@ -13,30 +15,92 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { FlatList } from "react-native-gesture-handler";
+
+
+// Datos que contendrá el ListView
+const dataSource = [
+    { id: 1, title: 'Reservar' },
+    
+];
+
 
 class MainMenu extends Component {
+
+    constructor(props) {
+        super(props);
+
+        
+        //dataSource.push({id:2, title:this.props.route.params.usuario});
+        
+    }
     
     
     render() {
 
+        const Item = ({ id, title }) => (
+
+            
+            <TouchableOpacity onPress={this.handleOnPress.bind(this, id)} style={styles.item}>
+                <Text style={styles.title}>{title}</Text>
+            </TouchableOpacity>
+
+            // <View style={styles.item}>
+            //   <Text style={styles.title}>{title}</Text>
+            // </View>
+        );
+
+        const renderItem = ({ item }) => (
+            <Item id={item.id} title={item.title} />
+        );
+
+
         return(
             <View>
-                <Text> Menu principal </Text>
+                <FlatList 
+                    data={dataSource}
+                    renderItem={renderItem}
+                    keyExtractor={item => item.id}
+                />
 
-                <TouchableOpacity onPress={this.handleOnPress}>
+                
+                {/* <Text> Menu principal </Text>  */}
+
+                {/* <TouchableOpacity onPress={this.handleOnPress.bind(this, "a")}>
                     <Text >Quien ha iniciado sesión</Text>
-                </TouchableOpacity>
+                </TouchableOpacity>  */}
             </View>
         );
     }
 
 
-    handleOnPress = () => {
-        console.log(this.props.route.params)
-        //console.log(this.props.navigation.state.params.usuario)
+    handleOnPress(parametro) {
+        console.log(parametro)
+        if(parametro == 1){
+            console.log("Navegación hacia reservar")
+            //this.props.navigation.navigate('PantallaReservas.js', {'usuario':this.state.email})
+        }
+        
+        
+        //console.log(this.props.route.params.usuario)
           
-      }
+    }
 }
 
 export default MainMenu
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      //marginTop: StatusBar.currentHeight || 0,
+    },
+    item: {
+      backgroundColor: 'gray',
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+    },
+    title: {
+      fontSize: 32,
+    },
+  });
